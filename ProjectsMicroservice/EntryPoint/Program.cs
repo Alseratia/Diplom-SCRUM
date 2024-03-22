1b711741cc7f1a9278a158b353c1729524f579a7
cassell <ccassell@student.21-school.ru>
2024-03-23 00:36:09 +0300

    Init repo

---
+++ ProjectsMicroservice/EntryPoint/Program.cs
@@ -0,0 +1,17 @@
+namespace AuthMicroservice;
+
+public static class Program
+{
+  public static void Main(string[] args)
+  {
+    var app = CreateHostBuilder(args).Build();
+    app.Run();
+  }
+
+  private static IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+      .ConfigureWebHostDefaults(webBuilder =>
+      {
+        webBuilder.UseStartup<Startup>();
+      });
+}
