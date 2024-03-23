1662836255f5823cfbe58c138fdfb56de9c8cae7
cassell <ccassell@student.21-school.ru>
2024-03-24 01:05:22 +0300

    DockerContainers

---
+++ AuthMicroservice/Infrastructure/PostgreSQL/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
+
+namespace AuthMicroservice.Infrastructure.PostgreSQL;
+
+public static class ServiceCollectionExtensions
+{
+
+  public static IServiceCollection AddDatabaseContext(this IServiceCollection services, string connectionString)
+  {
+    var dataSourceBuilder = new NpgsqlDataSourceBuilder(connectionString);
+    
+    services.AddDbContextFactory<ApplicationDbContext>(options => options.UseNpgsql(dataSourceBuilder.Build())
+      .UseSnakeCaseNamingConvention());
+    
+    return services;
+  }
+}
